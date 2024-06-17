d08196cc845310c589bc34a9348cfdb73211d0c8
Leandro Martínez <leandro.martinez17@gmail.com>
2024-06-17 18:34:02 -0300

    update: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import webpackNodeExternals from 'webpack-node-externals';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
@@ -10,15 +11,14 @@ export default {
         path: path.resolve(__dirname, 'dist'),
         filename: 'final.js',
     },
-    target: 'node',
     module: {
         rules: [
             {
-                test: /\.(?:js|mjs|cjs)$/,
-                exclude: `${__dirname}/node_modules/`,
-                loader: 'babel-loader'
+                test: /\.html$/i,
+                loader: "babel-loader",
             }
-
         ]
-    }
+    },
+    externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
+    externals: [webpackNodeExternals()], // in order to ignore all modules in node_modules folder
 };
